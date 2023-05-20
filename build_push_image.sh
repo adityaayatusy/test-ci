76f09596b8ece6ab9f1f19b5966b033e48d17c12
Aditya Ayatusy Syifa <adityaayatusy@gmail.com>
2023-05-20 14:30:19 +0700

    private token

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,6 +7,6 @@ docker image ls
 #mengubah nama image agar sesuai dengan format GitHub Packages
 docker tag item-app:v1 docker.pkg.github.com/adityaayatusy/a433-microservices/item-app:v1
 #login ke GitHub Packages
-docker login docker.pkg.github.com -u adityaayatusy -p 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
+docker login docker.pkg.github.com -u adityaayatusy --password-stdin < ~/github_token
 #mengunggah image ke GitHub Packages
 docker push docker.pkg.github.com/adityaayatusy/a433-microservices/item-app:v1
